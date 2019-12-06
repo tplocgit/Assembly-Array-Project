5ba90c9e1026b9764760d0980da275f84d475587
Lộc Trần Phước <giongnhuntlf@gmail.com>
2019-12-06 19:18:42 +0700

    Update: List all function must be done

--- DynamicArray.asm
+++ DynamicArray.asm
@@ -2,9 +2,24 @@
 strHello: .asciiz "Hello World!!!\n"
 .text
 main:
-addi $v0,$0,4
-la $a0,strHello
+addi $v0, $zero, 5 #$vo=5 to input an integer number, $v0 contain number read
 syscall
+add $t0,$zero,$v0 #$t0 = $v0 to save an integer inputed, $t0 contain size of array
+
+InputArray:
+
+OutputArray:
+
+SumArray:
+
+CheckPrime:
+
+FindMax:
+
+SearchX:
+
+PrintMenu:
+
 
 
 Exit:
