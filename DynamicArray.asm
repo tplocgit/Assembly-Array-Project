eda1e67408823c1439a54de91584091938d39092
Lộc Trần Phước <giongnhuntlf@gmail.com>
2019-12-07 09:57:16 +0700

    Update: Input Array, Allocate Array, Dellocate Array

--- DynamicArray.asm
+++ DynamicArray.asm
@@ -1,26 +1,116 @@
 .data
-strHello: .asciiz "Hello World!!!\n"
+# Menu string
+InputIntStr:	.asciiz "Please input an interger: "
+StartInputElmStr:	.asciiz "a["
+EndInputElmStr:	.asciiz "] = "
+OuputStr: 	.asciiz "Ouput: "
+SumStr:	.asciiz "Sum: "
+PrimeStr:	.asciiz "Prime: "
+MaxStr: 	.asciiz "Max: "
+endl:	.asciiz "\n"
 .text
 main:
-addi $v0, $zero, 5 #$vo=5 to input an integer number, $v0 contain number read
+# $s0 = size
+# $s1 = array pointer
+# Input size of array
+InputSize:
+# Print string to remind input integer
+li $v0, 4 # $v0 = 4 to print srting
+la $a0,  InputIntStr # $a0 = string to print
 syscall
-add $t0,$zero,$v0 #$t0 = $v0 to save an integer inputed, $t0 contain size of array
+# Input Size
+li $v0,  5 # $vo=5 to input an integer number, $v0 contain number read
+syscall
+# Check if n > 0
+CheckSizeLoop:
+sle $t0, $v0, $zero # if $v0(number inputed) > $0  => $t1 = 1 else $t1 = 0
+bnez $t0, InputSize # if $t1 = 0  => $v0 !> 0 =>$v0 <= 0 => goto InputSize <=> while(n<=0) cin >> n
+EndCheckSizeLoop:
+move $s0, $v0 # $s0 = $v0 to save an integer inputed, $t0 contain size of array
+EndInputSize:
+# Input array
+jal InputArray
 
-InputArray:
+jal DellocateArray
+EndMain:
+j Exit
+# Function Part
+# $tx is template res so just use it =))
+# $s0 contain size
+# $s1 contian Adress of array
+InputArray: 
+# Allocate Heap memory, int* a = new int[n]
+li $v0, 9 # v0 = 9 to allocate memory
+li $t0, 4 # t0 = 4 byte
+mult $s0, $t0 # calculate size of memory n * 4
+mflo $a0 # a0 = size calculated
+syscall # allocate, (a0 = size * 4) bytes, v0 contain adress
+move $s1, $v0 # s1 = v0, Contain array pointer 
+move $t0, $s1 # save pointer
+# Input data
+li $t1, 0 # Init $t01= i = 0
+InputArrayLoop: # Start Loop
+# Codision
+sge $t2,  $t1,  $s0 # if ($t1 >= $s0) => (i >= n) => $t2 = 1 else $t2 = 0
+bnez $t2, EndInputArrayLoop # (t2 != 0) => ($t1 = 1) => break
+
+#Print string: "a[i] = "
+li $v0,  4 # Print string
+la $a0, StartInputElmStr # a0 = value to print
+syscall
+li $v0,  1 # print integer
+move $a0, $t2  # a0 = value to print
+syscall
+li $v0,  4 # Print string
+la $a0, EndInputElmStr # a0 = value to print
+syscall
+
+# Read integer as element
+li $v0, 5 # Read num, v0 contain it
+syscall
+sw $v0, 0($t0) # Save inputed to arr
+addi $t0, $t0, 4 # get next position
+
+# i++
+addi $t1, $t1, 1
+j InputArrayLoop
+
+EndInputArrayLoop:
+EndInputArray: 
+jr $ra # return
 
 OutputArray:
+EndOuputArray:
+jr $ra
 
 SumArray:
+EndSumArray:
+jr $ra
 
 CheckPrime:
+EndCheckPrime:
+jr $ra
 
 FindMax:
+EndFindMax:
+jr $ra
 
 SearchX:
+EndSearchX:
+jr $ra
 
 PrintMenu:
+EndPrintMenu:
+jr $ra
 
-
+DellocateArray:
+li $v0, 9 # v0 = 9 to allocate memory
+li $t0, -4 # t0 = 4 byte
+mult $s0, $t0 # calculate size of memory n * 4
+mflo $a0 # a0 = size calculated
+syscall # allocate, (a0 = size * 4) bytes, v0 contain adress
+EndDellocateArray:
+jr $ra
 
 Exit:
 addi $v0,$0,10
